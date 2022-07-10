45649789ca8d82d49d27dc81d04b74ee397086a2
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-07-10 19:55:24 +0900

    Update package.sh

--- redpill-qjs-dtb/src/package.sh
+++ redpill-qjs-dtb/src/package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-tar --owner=root --group=root -czf patch.tar.gz --mode='+x' dtc dts.js
+tar --owner=root --group=root -czf patch.tar.gz --mode='+x' dtc dts.js dts_fs.js
 tar --owner=root --group=root -czf ../releases/package.tgz --mode='+x' install_rd.sh install.sh patch.tar.gz *.dts
 sha256sum ../releases/package.tgz
