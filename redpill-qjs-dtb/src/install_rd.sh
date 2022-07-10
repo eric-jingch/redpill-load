050cb8f8f8177e2f3f5a750d3f8271eb13407ca3
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-07-10 19:47:37 +0900

    Update install_rd.sh

--- redpill-qjs-dtb/src/install_rd.sh
+++ redpill-qjs-dtb/src/install_rd.sh
@@ -7,15 +7,15 @@ cp dtc /usr/sbin/dtc
 
 # copy 16 ports dts file to /etc.defaults
 echo $PLATFORM_ID
-if [ "${PLATFORM_ID}" = "ds920p_42218" ]; then
+if [ "${PLATFORM_ID}" == "ds920p_42218" ]; then
   echo "copy DS920+'s 16 ports ds920p_16ports.dts to /etc.defaults"
   ./dtc -I dts -O dtb -o model.dtb ds920p_16ports.dts
   cp -vf model.dtb /etc.defaults/model.dtb
-elif [ "${PLATFORM_ID}" = "ds918p_42218" ]; then
+elif [ "${PLATFORM_ID}" == "ds918p_42218" ]; then
   echo "copy DS918+'s 16 ports ds918p_16ports.dts to /etc.defaults"
   ./dtc -I dts -O dtb -o model_ds918p_42218.dtb ds918p_16ports.dts
   cp -vf model.dtb /etc.defaults/model.dtb
-elif [ "${PLATFORM_ID}" = "ds1621p_42218" ]; then
+elif [ "${PLATFORM_ID}" == "ds1621p_42218" ]; then
   echo "copy DS1621+'s 16 ports ds1621p_16ports.dts to /etc.defaults"
   ./dtc -I dts -O dtb -o model.dtb ds1621p_16ports.dts
   cp -vf model.dtb /etc.defaults/model.dtb
@@ -26,7 +26,11 @@ if [ ! -f model_${PLATFORM_ID}.dtb ]; then
 echo "dtb file not exists for this platform!!!"
   # Dynamic generation
   ./dtc -I dtb -O dts -o output.dts /etc.defaults/model.dtb
-  qjs --std ./dts.js output.dts output.dts.out
+  if [ "echo ${PLATFORM_ID} | cut -c 1-2" == "fs" ]; then
+    qjs --std ./dts_fs.js output.dts output.dts.out
+  else
+    qjs --std ./dts.js output.dts output.dts.out 
+  fi
   if [ $? -ne 0 ]; then
     echo "auto generated dts file is broken"
     exit 0
